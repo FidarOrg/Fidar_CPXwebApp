583ac95a82f69c7b5fb102da131b181c1ce9730f
Syed Thousif <syedthousift97@gmail.com>
2026-04-16 17:41:49 +0530

    cpx-changes

--- src/components/qrpage.jsx
+++ src/components/qrpage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import logo from "../assets/banklogo.png";
+import logo from "../assets/cpx.png";
 import ModeToggle from "@/components/theme-provider/mode-toggle";
 
 import {
