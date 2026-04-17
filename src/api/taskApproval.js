88eaef9f036a3acfd17020640b09071f3117fee5
Naveed-23 <naveedbh23@gmail.com>
2026-04-17 12:53:47 +0530

    getting userid from sdk

--- src/api/taskApproval.js
+++ src/api/taskApproval.js
@@ -13,9 +13,9 @@ function getFidarUserId(profile) {
 }
 
 async function approveWithFidarPasskey(task) {
-  // const profile = await fidar.getMyProfile();
-  // const userId = getFidarUserId(profile);
-  const userId = "0fb02b54-117d-4a0a-813c-767ac15d5cee";
+  const profile = await fidar.getMyProfile();
+  const userId = getFidarUserId(profile);
+  // const userId = "0fb02b54-117d-4a0a-813c-767ac15d5cee";
 
   if (!userId) {
     throw new Error("Unable to resolve the FIDAR user identity for signing.");
